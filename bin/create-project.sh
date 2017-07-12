013368de381bdccccc79c474ea8ac65229549366
Carlos Meyer <cm@davitec.de>
2017-07-12 18:25:08 +0200

    fix typo

--- bin/create-project.sh
+++ bin/create-project.sh
@@ -22,8 +22,8 @@ case "$1" in
     ###################################
     "typo3")
         #execInDir "$CODE_DIR" "docker run --rm --env COMPOSER_CACHE_DIR=/tmp --user $(id -u):$(id -g) -v \$(pwd):/app composer/composer:alpine create-project typo3/cms-base-distribution /app"
-        execInDir "$CODE_DIR" "ln -s vendor/bin/typo3cms"
-        execInDir "$CODE_DIR" "chmod +x typo3cms"
+        #execInDir "$CODE_DIR" "ln -s vendor/bin/typo3cms"
+        #execInDir "$CODE_DIR" "chmod +x typo3cms"
         execInDir "$CODE_DIR" "docker run --rm --user $(id -u):$(id -g) --net=t3docker_default --link t3docker_mysql_1:mysql -v \$(pwd):/app t3docker_app /bin/bash -c '\
         ./typo3cms install:setup --non-interactive \
         --database-user-name=dev \
@@ -60,10 +60,10 @@ case "$1" in
         ./typo3cms extension:activate lowlevel; \
         cp vendor/typo3/cms/_.htaccess web/.htaccess; \
         ./typo3cms autocomplete; \
-        ./typo3cms database:updateschema "*.*"; \
+        ./typo3cms database:updateschema \"*.*\"; \
         ./typo3cms cleanup:updatereferenceindex; \
         ./typo3cms cache:flush; \
-        echo "8x ready"'"
+        echo \"8x ready\"'"
         ;;
 
     ###################################
