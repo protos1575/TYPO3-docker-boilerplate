0ca1c2e969096a160b26926eef05ae09efffa3f7
Carlos Meyer <cm@davitec.de>
2017-07-12 17:57:25 +0200

    install t3 with t3console

--- bin/create-project.sh
+++ bin/create-project.sh
@@ -22,7 +22,42 @@ case "$1" in
     ###################################
     "typo3")
         execInDir "$CODE_DIR" "docker run --rm --env COMPOSER_CACHE_DIR=/tmp --user $(id -u):$(id -g) -v \$(pwd):/app composer/composer:alpine create-project typo3/cms-base-distribution /app"
-        execInDir "$CODE_DIR" "touch web/FIRST_INSTALL"
+        execInDir "$CODE_DIR" "chmod +x typo3cms"
+        execInDir "$CODE_DIR" "ln -s vendor/bin/typo3cms"
+        execInDir "$CODE_DIR" "docker run --rm --user $(id -u):$(id -g) --net=t3docker_default --link t3docker_mysql_1:mysql -v \$(pwd):/app t3docker_app /bin/bash -c '\
+        ./typo3cms install:setup --non-interactive \
+        --database-user-name=dev \
+        --database-user-password=dev \
+        --database-host-name=mysql \
+        --database-port=3306 \
+        --database-name=typo3 \
+        --admin-user-name=dev-admin \
+        --admin-password=admin123  \
+        --site-name=TYPO3 8x; \
+        ./typo3cms extension:activate beuser; \
+        ./typo3cms extension:activate scheduler; \
+        ./typo3cms extension:activate rte_ckeditor; \
+        ./typo3cms extension:activate context_help; \
+        ./typo3cms extension:activate viewpage; \
+        ./typo3cms extension:activate func; \
+        ./typo3cms extension:activate wizard_crpages; \
+        ./typo3cms extension:activate wizard_sortpages; \
+        ./typo3cms extension:activate info; \
+        ./typo3cms extension:activate info_pagetsconfig; \
+        ./typo3cms extension:activate tstemplate; \
+        ./typo3cms extension:activate fluid_styled_content; \
+        ./typo3cms extension:activate cshmanual; \
+        ./typo3cms extension:activate documentation; \
+        ./typo3cms extension:activate about; \
+        ./typo3cms extension:activate t3editor; \
+        ./typo3cms extension:activate sys_note; \
+        ./typo3cms extension:activate rsaauth; \
+        ./typo3cms extension:activate reports; \
+        ./typo3cms extension:activate recycler; \
+        ./typo3cms extension:activate reports; \
+        ./typo3cms extension:activate opendocs; \
+        ./typo3cms extension:activate setup; \
+        ./typo3cms extension:activate lowlevel'"
         ;;
 
     ###################################
